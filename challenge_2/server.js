b6aa61b9d77972bc4a5729d98c25bac5c75c5e82
Philipp Albrecht <philippbalbrecht@gmail.com>
2018-10-26 06:27:03 -0700

    [wip] set up route

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser')
 
+const app = express();
+
+
+
+// parse application/json
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+ 
+
+
+
+app.use(express.static('client'))
+
+
+app.post('/jsontext', (req, res) => {
+  
+  const  json = JSON.parse(req.body.data);
+
+  let csv;
+
+
+  
+
+  res.redirect('/')
+})
+
+const port = 9500;
+
+app.listen(port, ()=>{
+  console.log(`Listening on port ${port}`)
+})
 
